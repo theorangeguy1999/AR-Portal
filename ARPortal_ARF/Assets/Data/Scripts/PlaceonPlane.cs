169048efcfa594b2ab7d2f26f1f717721a8acf8d
theorangeguy1999 <sankalp.sharma99@gmail.com>
2020-07-24 03:35:36 +0530

    Pushing Project

---
+++ ARPortal_ARF/Assets/Data/Scripts/PlaceonPlane.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlaceonPlane : MonoBehaviour
+{
+    public GameObject Portal;
+    public GameObject M_Cam;
+
+    private ARRaycastManager _arraycastmanager;
+    private Vector2 touchpos;
+
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private void Awake() 
+    {
+        _arraycastmanager = GetComponent<ARRaycastManager>();
+    }
+
+    bool TryGetTouchPos(out Vector2 touchpos)
+    {
+        if(Input.touchCount > 0)
+        {
+            touchpos = Input.GetTouch(0).position;
+            return true;
+        }
+
+        touchpos = default;
+        return false;
+    }
+    void Update()
+    {
+        if(!TryGetTouchPos(out touchpos))
+            return;
+        if(_arraycastmanager.Raycast(touchpos,hits,TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = hits[0].pose;
+            //Anchor anchor = hits[0].Trackable.CreateAnchor(hits[0].pose);
+            Portal.SetActive(true);
+
+            Portal.transform.position = hits[0].pose.position;
+            Portal.transform.rotation = hits[0].pose.rotation;
+
+            Vector3 cam_pos = M_Cam.transform.position;
+
+            cam_pos.y = hits[0].pose.position.y;
+
+            Portal.transform.LookAt(cam_pos,Portal.transform.up);
+            //Portal.transform.parent = anchor.transform;
+
+        }
+        
+    }
+}
---
+++ ARPortal_ARF/Assets/Data/Scripts/PortalManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PortalManager : MonoBehaviour
+{
+    public GameObject MCam;
+    public GameObject Sponza;
+
+    private Material[] SponzaMat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SponzaMat = Sponza.GetComponent<Renderer>().sharedMaterials;
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay(Collider collider)
+    {
+
+        Debug.Log("Collision");
+        Vector3 CamPosinPSpace = transform.InverseTransformPoint(MCam.transform.position);
+
+        if(CamPosinPSpace.y < 0.5f)
+        {
+           for(int i = 0 ; i<SponzaMat.Length; i++)
+           {
+               Debug.Log("value set");
+               SponzaMat[i].SetInt("_StencilComp",(int)CompareFunction.Always);
+           } 
+        }
+        else
+        {
+            for(int i = 0 ; i<SponzaMat.Length; i++)
+           {
+                Debug.Log("value unset");
+               SponzaMat[i].SetInt("_StencilComp",(int)CompareFunction.Equal);
+           } 
+
+        }
+        
+    }
+}
